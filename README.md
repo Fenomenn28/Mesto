9930b2175bc43037181c8abba05e250a2c89249c
Alexander rogulin <alexander_rogulin@bk.ru>
2023-05-19 15:44:15 +0300

    Исправил ошибки

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 Mesto
 
-отличия от прошлого проекта: Подготовка к JS. добавление poput
+отличия от прошлого проекта: 
+Был прописан Js-код, появились возможности: 
+редактировать профиль, добавлять контент и рассматривать изображения в полный рост.
+В html был добавлен template и 2 новых popup 
+В CSS была добавлена анимация для popup
 
-Технологии: flex, grid, псевдоклассы, медиа запросы  
+Технологии: classList, template, querySelector, function, addEventListener, массив и forEach для работы с ним,
+cloneNode, Объект event, переменную const 
+Инструменты для работы с DOM такие как: prepend, textContent, src
+
 
 https://fenomenn28.github.io/Mesto/
+
--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 const formElementProfile = document.querySelector('[name="profile-form"]');
 const profileOpenPopup = document.querySelector('.profile__edit-button');
 const profileClosePopup = document.querySelector('.close-profile');
-const popupProfile = document.querySelector('.popup__profile');
+const popupProfile = document.querySelector('.popup_profile');
 const nameInput = document.querySelector('[name="name"]');
 const jobInput = document.querySelector('[name="profession"]');
-
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
 // открыть или закрыть Popup профиля
-function OpenOrCloseProfile() {
+function openOrCloseProfile() {
     nameInput.value = '';
     jobInput.value = '';
     popupProfile.classList.toggle('popup_opened');
@@ -16,29 +17,28 @@ function OpenOrCloseProfile() {
 function handleFormSubmit(evt) {
 evt.preventDefault(); 
 
-    const profileName = document.querySelector('.profile__name');
-    const profileProfession = document.querySelector('.profile__profession');
-
     profileName.textContent = nameInput.value;
     profileProfession.textContent = jobInput.value;
 
     popupProfile.classList.remove('popup_opened');
 }
 
-profileOpenPopup.addEventListener('click', OpenOrCloseProfile);
-profileClosePopup.addEventListener('click', OpenOrCloseProfile);
+profileOpenPopup.addEventListener('click', openOrCloseProfile);
+profileClosePopup.addEventListener('click', openOrCloseProfile);
 formElementProfile.addEventListener('submit', handleFormSubmit);
 //_________________________________________________________
 //Карты 
 const cards = document.querySelector('.cards');
-const popupCards = document.querySelector('.popup__cards')
+const popupCards = document.querySelector('.popup_cards')
 const cardsOpenPopup = document.querySelector('.profile__add-button');
 const cardsClosePopup = document.querySelector('.close-cards');
 const formElementCards = document.querySelector('[name="cards-form"]');
 const titleInput = document.querySelector('[name="title"]');
 const imageInput = document.querySelector('[name="image"]');
 const popupImage = document.querySelector('.popup-image');
-
+const imagePopupImage = document.querySelector('.popup-image__image');
+const namePopupImage = document.querySelector('.popup-image__name');
+const cardTemplate = document.querySelector('#card').content;
 //создание первых 6 карт 
 const initialCards = [
     {
@@ -66,7 +66,6 @@ const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ].forEach(function(item){
-    const cardTemplate = document.querySelector('#card').content;
     const cardTemplateClone = cardTemplate.querySelector('.cards__card').cloneNode(true);
     const cardImage = cardTemplateClone.querySelector('.cards__image');
     const cardName = cardTemplateClone.querySelector('.cards__name');
@@ -79,20 +78,19 @@ const initialCards = [
     });
     //delete
     cardTemplateClone.querySelector('.cards__delete-cards').addEventListener('click',function(evt){
-        evt.target.parentElement.remove();
+        evt.target.closest('.cards__card').remove();
     });
-
+    //popupImage
     cardImage.addEventListener('click',function(evt){
         document.querySelector('.popup-image').classList.add('popup-image_opened');
-        document.querySelector('.popup-image__image').src = evt.target.src;
-        document.querySelector('.popup-image__name').textContent = cardName.textContent;
-        
+        imagePopupImage.src = evt.target.src;
+        namePopupImage.textContent = cardName.textContent;
     });
 
     cards.append(cardTemplateClone);
 });
 //открыть или закрыть Popup добавления карт
-function OpenOrCloseCards () {
+function openOrCloseCards () {
     titleInput.value = '';
     imageInput.value = '';
     popupCards.classList.toggle('popup_opened');
@@ -100,10 +98,7 @@ function OpenOrCloseCards () {
 //Добавление карт
 function handleFormSubmitForCards(evt) {
 evt.preventDefault(); 
-
-    const cardTemplate = document.querySelector('#card').content;
     const cardTemplateClone = cardTemplate.querySelector('.cards__card').cloneNode(true);
-
     const cardName = cardTemplateClone.querySelector('.cards__name');
     const cardImage = cardTemplateClone.querySelector('.cards__image');
     cardName.textContent = titleInput.value;
@@ -134,8 +129,8 @@ document.querySelector('.close-image').addEventListener('click', function() {
     popupImage.classList.remove('popup-image_opened');
 });
 
-cardsOpenPopup.addEventListener('click', OpenOrCloseCards);
-cardsClosePopup.addEventListener('click', OpenOrCloseCards);
+cardsOpenPopup.addEventListener('click', openOrCloseCards);
+cardsClosePopup.addEventListener('click', openOrCloseCards);
 formElementCards.addEventListener('submit', handleFormSubmitForCards);
 
 
